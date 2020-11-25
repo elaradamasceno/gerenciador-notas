913efc6cb1717690dd9425909fe82d8062b3c82e
elaradamasceno <elaradamasceno.edl@gmail.com>
2020-11-24 21:33:26 -0300

    react - class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import NoteList from './components/NoteList/NoteList';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import ListCategories from './components/ListCategories/ListCategories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './assets/App.css';
+import './assets/index.css';
+import Categories from './data/Categories';
+import ArrayNotes from './data/ArrayNotes';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.categories = new Categories();
+        this.notes = new ArrayNotes();
+    }
+
+    render(){
+        return (
+            <section className="content">
+                <RegistrationForm 
+                    createNote={this.notes.createNote.bind(this.notes)}
+                    categories={this.categories}>
+                </RegistrationForm>
+
+                <main className="main-content">
+                    <ListCategories 
+                        categories={this.categories} 
+                        addCategorie={this.categories.createCategory.bind(this.categories)}>
+                    </ListCategories>
+
+                    <NoteList 
+                        deleteNote={this.notes.deleteNote.bind(this.notes)}
+                        notes={this.notes}>
+                    </NoteList>
+                </main>
+
+            </section>
+        );
+    }
 }
 
 export default App;
